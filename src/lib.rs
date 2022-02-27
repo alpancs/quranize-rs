e55a1069cac27c1091c5cc101bd5bdcb310204a8
Alfan Nur Fauzan <alpancs@gmail.com>
2022-02-28 02:10:48 +0700

    index word position

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@ use xml::EventReader;
 
 mod quran_simple_clean;
 
-pub fn build_location_map() -> Vec<(u8, u16)> {
+pub fn build_location_map() -> Vec<(u8, u16, u8)> {
     let mut sura = 0;
     let mut location_map = vec![];
     for event in EventReader::new(quran_simple_clean::RAW_XML.as_bytes()) {
@@ -16,8 +16,17 @@ pub fn build_location_map() -> Vec<(u8, u16)> {
                     sura += 1;
                 }
                 "aya" => {
-                    let aya = get_index_from_attributes(attributes);
-                    location_map.push((sura, aya));
+                    let aya_number = get_index_from_attributes(&attributes);
+                    let mut word_index = 0;
+                    location_map.push((sura, aya_number, word_index));
+
+                    let aya_text = get_text_from_attributes(&attributes);
+                    for c in aya_text.chars() {
+                        if c == ' ' {
+                            word_index += 1;
+                            location_map.push((sura, aya_number, word_index));
+                        }
+                    }
                 }
                 _ => {}
             },
@@ -30,7 +39,7 @@ pub fn build_location_map() -> Vec<(u8, u16)> {
     location_map
 }
 
-fn get_index_from_attributes(attributes: Vec<OwnedAttribute>) -> u16 {
+fn get_index_from_attributes(attributes: &Vec<OwnedAttribute>) -> u16 {
     attributes
         .iter()
         .find(|a| a.name.to_string() == "index")
@@ -40,6 +49,15 @@ fn get_index_from_attributes(attributes: Vec<OwnedAttribute>) -> u16 {
         .unwrap()
 }
 
+fn get_text_from_attributes(attributes: &Vec<OwnedAttribute>) -> String {
+    attributes
+        .iter()
+        .find(|a| a.name.to_string() == "text")
+        .unwrap()
+        .value
+        .to_string()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -47,8 +65,8 @@ mod tests {
     #[test]
     fn test_build_location_map() {
         let location_map = build_location_map();
-        assert_eq!(location_map.len(), 6236);
-        assert_eq!(*location_map.first().unwrap(), (1, 1));
-        assert_eq!(*location_map.last().unwrap(), (114, 6));
+        assert_eq!(location_map.len(), 77800);
+        assert_eq!(*location_map.first().unwrap(), (1, 1, 0));
+        assert_eq!(*location_map.last().unwrap(), (114, 6, 2));
     }
 }
