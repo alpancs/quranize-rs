68427369eba759c9e719a2ed0ad8a92c5eb18f5a
Alfan Nur Fauzan <alpancs@gmail.com>
2022-03-09 18:07:12 +0700

    refactor

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,29 @@
 #![feature(test)]
 
 mod quran_index;
+use quran_index::{build_quran_index, Harf};
 
 mod quranize_map;
+use quranize_map::{build_quranize_map, QuranizeMap};
 
 pub struct Quranize {
-    _quran_index: quran_index::Harf,
-    _quranize_map: quranize_map::QuranizeMap,
+    quran_index: Harf,
+    quranize_map: QuranizeMap,
 }
 
 pub fn build_quranize() -> Quranize {
     Quranize {
-        _quran_index: quran_index::build_quran_index(),
-        _quranize_map: quranize_map::build_quranize_map(),
+        quran_index: build_quran_index(),
+        quranize_map: build_quranize_map(),
     }
 }
 
 impl Quranize {
-    pub fn encode(_: &str) -> String {
+    pub fn encode(&self, alphabet: &str) -> String {
+        self.encode_with_context(alphabet, &self.quran_index)
+    }
+
+    fn encode_with_context(&self, alphabet: &str, context: &Harf) -> String {
         String::new()
     }
 }
