7703d209e2d3d9c63546d919399ec4933b001290
Alfan Nur Fauzan <alpancs@gmail.com>
2022-03-10 11:50:05 +0700

    implement encode function

--- src/lib.rs
+++ src/lib.rs
@@ -18,12 +18,63 @@ pub fn build_quranize() -> Quranize {
     }
 }
 
+type EncodeResult = Vec<(String, Vec<(u8, u16, u8)>)>;
+
 impl Quranize {
-    pub fn encode(&self, alphabet: &str) -> String {
-        self.encode_with_context(alphabet, &self.quran_index)
+    pub fn encode(&self, text: &str) -> EncodeResult {
+        self.encode_with_context(text, &self.quran_index)
     }
 
-    fn encode_with_context(&self, alphabet: &str, context: &Harf) -> String {
-        String::new()
+    fn encode_with_context(&self, text: &str, context: &Harf) -> EncodeResult {
+        let mut results = Vec::new();
+        for (alphabet, qurans) in self.quranize_map.iter() {
+            if !text.starts_with(alphabet) {
+                continue;
+            }
+            for quran in qurans {
+                for harf in context.next_harfs.iter() {
+                    if *quran == harf.content {
+                        let subtext = &text[alphabet.len()..];
+                        if subtext.is_empty() {
+                            if !harf.locations.is_empty() {
+                                results.push((quran.to_string(), harf.locations.to_vec()));
+                            }
+                        } else {
+                            let subresults = self.encode_with_context(subtext, harf);
+                            results.append(
+                                &mut subresults
+                                    .into_iter()
+                                    .map(|(q, l)| (quran.to_string() + &q, l))
+                                    .collect(),
+                            );
+                        }
+                        break;
+                    }
+                }
+            }
+        }
+        results
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_quranize() {
+        let quranize = build_quranize();
+        assert_eq!(
+            quranize
+                .encode("bismi")
+                .iter()
+                .map(|(q, _)| q)
+                .collect::<Vec<_>>(),
+            "بإثمي بئسما باسم بعصم بسم"
+                .split_whitespace()
+                .collect::<Vec<_>>(),
+        );
+        assert_eq!(quranize.encode("").len(), 0);
+        assert_eq!(quranize.encode("mxkasmxka").len(), 0);
     }
 }
--- src/quranize_map.rs
+++ src/quranize_map.rs
@@ -1,53 +1,50 @@
-pub type QuranizeMap = Vec<(String, Vec<String>)>;
+pub type QuranizeMap = Vec<(String, Vec<char>)>;
 
 pub fn build_quranize_map() -> QuranizeMap {
     let transliteration_map_pairs = [
-        ("ء", "' k a i u"),
-        ("آ", "a aa"),
-        ("أ", "a u ' k"),
-        ("ؤ", "' k u"),
-        ("إ", "i"),
-        ("ئ", "' k i"),
-        ("ا", "a i u"),
-        ("ب", "b ba bi bu"),
-        ("ة", "h ta ti tu t"),
-        ("ت", "t ta ti tu"),
-        ("ث", "ts tsa tsi tsu s sa si su"),
-        ("ج", "j ja ji ju"),
-        ("ح", "h ha hi hu ch cha chi chu kh kha khi khu"),
-        ("خ", "kh kho khi khu h ho hi hu kha ha"),
-        ("د", "d da di du"),
-        ("ذ", "d da di du dh dha dhi dhu dz dza dzi dzu"),
-        ("ر", "r ro ri ru ra"),
-        ("ز", "z za zi zu"),
-        ("س", "s sa si su"),
-        ("ش", "s sa si su sy sya syi syu sh sha shi shu"),
-        ("ص", "s so si su sh sho shi shu sa sha"),
-        ("ض", "d do di du dh dho dhi dhu dz dzo dzi dzu"),
-        ("ط", "t to ti tu th tho thi thu ta tha"),
-        ("ظ", "d do di du dh dho dhi dhu dz dzo dzi dzu"),
-        ("ع", "' 'a 'i 'u k a i u"),
-        ("غ", "g go gi gu gh gho ghi ghu ga gha"),
-        ("ف", "f fa fi fu"),
-        ("ق", "k ko ki ku q qo qi qu qa"),
-        ("ك", "k ka ki ku"),
-        ("ل", "l la li lu"),
-        ("م", "m ma mi mu"),
-        ("ن", "n na ni nu"),
-        ("ه", "h ha hi hu"),
-        ("و", "w wa wi wu u"),
-        ("ى", "a"),
-        ("ي", "y ya yi yu i iya iyi iyu"),
-        ("ال", "a l la"),
+        ('ء', "' k a i u"),
+        ('آ', "a aa"),
+        ('أ', "a u ' k"),
+        ('ؤ', "' k u"),
+        ('إ', "i"),
+        ('ئ', "' k i"),
+        ('ا', "a i u"),
+        ('ب', "b ba bi bu"),
+        ('ة', "h ta ti tu t"),
+        ('ت', "t ta ti tu"),
+        ('ث', "ts tsa tsi tsu s sa si su"),
+        ('ج', "j ja ji ju"),
+        ('ح', "h ha hi hu ch cha chi chu kh kha khi khu"),
+        ('خ', "kh kho khi khu h ho hi hu kha ha"),
+        ('د', "d da di du"),
+        ('ذ', "d da di du dh dha dhi dhu dz dza dzi dzu"),
+        ('ر', "r ro ri ru ra"),
+        ('ز', "z za zi zu"),
+        ('س', "s sa si su"),
+        ('ش', "s sa si su sy sya syi syu sh sha shi shu"),
+        ('ص', "s so si su sh sho shi shu sa sha"),
+        ('ض', "d do di du dh dho dhi dhu dz dzo dzi dzu"),
+        ('ط', "t to ti tu th tho thi thu ta tha"),
+        ('ظ', "d do di du dh dho dhi dhu dz dzo dzi dzu"),
+        ('ع', "' 'a 'i 'u k a i u"),
+        ('غ', "g go gi gu gh gho ghi ghu ga gha"),
+        ('ف', "f fa fi fu"),
+        ('ق', "k ko ki ku q qo qi qu qa"),
+        ('ك', "k ka ki ku"),
+        ('ل', "l la li lu"),
+        ('م', "m ma mi mu"),
+        ('ن', "n na ni nu"),
+        ('ه', "h ha hi hu"),
+        ('و', "w wa wi wu u"),
+        ('ى', "a"),
+        ('ي', "y ya yi yu i iya iyi iyu"),
     ];
     let mut quranize_map = QuranizeMap::new();
     for (quran, alphabets) in transliteration_map_pairs {
         for alphabet in alphabets.split_whitespace() {
-            let quran = quran.to_string();
-            let alphabet = alphabet.to_string();
             match quranize_map.iter_mut().find(|(k, _)| *k == alphabet) {
                 Some((_, qurans)) => qurans.push(quran),
-                None => quranize_map.push((alphabet, vec![quran])),
+                None => quranize_map.push((alphabet.to_string(), vec![quran])),
             }
         }
     }
@@ -61,16 +58,20 @@ mod tests {
     #[test]
     fn test_build_quranize_map() {
         let quranize_map = build_quranize_map();
-        assert_eq!(join_values_by_key(&quranize_map, "ba"), "ب");
-        assert_eq!(join_values_by_key(&quranize_map, "ku"), "ق ك");
-        assert_eq!(join_values_by_key(&quranize_map, "'"), "ء أ ؤ ئ ع");
+        assert_eq!(find_values(&quranize_map, "ba"), split_to_chars("ب"));
+        assert_eq!(find_values(&quranize_map, "ku"), split_to_chars("ق ك"));
+        assert_eq!(find_values(&quranize_map, "'"), split_to_chars("ء أ ؤ ئ ع"));
     }
 
-    fn join_values_by_key(quranize_map: &QuranizeMap, key: &str) -> String {
+    fn find_values(quranize_map: &QuranizeMap, key: &str) -> Vec<char> {
         quranize_map
             .iter()
             .find_map(|(k, v)| if k == key { Some(v) } else { None })
             .unwrap()
-            .join(" ")
+            .to_vec()
+    }
+
+    fn split_to_chars(text: &str) -> Vec<char> {
+        text.chars().filter(|c| *c != ' ').collect()
     }
 }
