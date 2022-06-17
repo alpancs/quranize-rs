064da180fcafe66d65d7c9cadd427ba8c9a59011
Alfan Nur Fauzan <alpancs@gmail.com>
2022-06-18 00:02:28 +0700

    validate basmalah prefixes

--- src/quranize/quran/mod.rs
+++ src/quranize/quran/mod.rs
@@ -11,6 +11,7 @@ mod tests {
     #[test]
     fn validate_quran_versions() {
         assert_eq!(count_words(SIMPLE_CLEAN), count_words(SIMPLE_PLAIN));
+        assert_eq!(trimmed_basmalah(SIMPLE_PLAIN), 112);
     }
 
     fn count_words(raw_quran: &str) -> usize {
@@ -20,5 +21,14 @@ mod tests {
             .take_while(|l| !l.is_empty())
             .map(|l| l.split('|').nth(2).unwrap().split_whitespace().count())
             .sum()
+    }
+
+    fn trimmed_basmalah(raw_quran: &str) -> usize {
+        let mut lines = raw_quran.trim_start().split('\n');
+        let basmalah = lines.next().unwrap().split('|').nth(2).unwrap();
+        lines
+            .take_while(|l| !l.is_empty())
+            .filter_map(|l| l.split('|').nth(2).unwrap().strip_prefix(basmalah))
+            .count()
     }
 }
