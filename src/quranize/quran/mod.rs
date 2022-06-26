86b18d9aa7d8362812036f3942c92900349e715b
Alfan Nur Fauzan <alpancs@gmail.com>
2022-06-26 15:11:50 +0700

    update test

--- src/quranize/quran/mod.rs
+++ src/quranize/quran/mod.rs
@@ -10,25 +10,30 @@ mod tests {
 
     #[test]
     fn validate_quran_versions() {
-        assert_eq!(count_words(SIMPLE_CLEAN), count_words(SIMPLE_PLAIN));
-        assert_eq!(trimmed_basmalah(SIMPLE_PLAIN), 112);
+        assert_eq!(word_counts(SIMPLE_CLEAN), word_counts(SIMPLE_PLAIN));
+        assert_same_basmalah(SIMPLE_CLEAN);
+        assert_same_basmalah(SIMPLE_PLAIN);
     }
 
-    fn count_words(raw_quran: &str) -> usize {
-        raw_quran
-            .trim_start()
+    fn word_counts(raw: &str) -> Vec<usize> {
+        raw.trim_start()
             .split('\n')
             .take_while(|l| !l.is_empty())
             .map(|l| l.split('|').nth(2).unwrap().split_whitespace().count())
-            .sum()
+            .collect()
     }
 
-    fn trimmed_basmalah(raw_quran: &str) -> usize {
-        let mut lines = raw_quran.trim_start().split('\n');
+    fn assert_same_basmalah(raw: &str) {
+        let mut lines = raw.trim_start().split('\n').take_while(|l| !l.is_empty());
         let basmalah = lines.next().unwrap().split('|').nth(2).unwrap();
-        lines
-            .take_while(|l| !l.is_empty())
-            .filter_map(|l| l.split('|').nth(2).unwrap().strip_prefix(basmalah))
-            .count()
+        for line in lines {
+            let mut parts = line.split('|');
+            let sura_number: u8 = parts.next().unwrap().parse().unwrap();
+            let aya_number: u16 = parts.next().unwrap().parse().unwrap();
+            let aya_text = parts.next().unwrap();
+            if aya_number == 1 && sura_number != 9 {
+                assert!(aya_text.starts_with(basmalah))
+            }
+        }
     }
 }
