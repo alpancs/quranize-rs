c07a06295684196d574ba938f1875fb6d34f642c
Alfan Nur Fauzan <alpancs@gmail.com>
2022-06-28 16:58:29 +0700

    refactor test

--- src/quranize/quran/mod.rs
+++ src/quranize/quran/mod.rs
@@ -7,11 +7,10 @@ pub use simple_plain::SIMPLE_PLAIN;
 #[cfg(test)]
 mod tests {
     use super::*;
-    use std::iter::zip;
 
     #[test]
     fn validate_quran_versions() {
-        for (wc_a, wc_b) in zip(word_counts(SIMPLE_CLEAN), word_counts(SIMPLE_PLAIN)) {
+        for (wc_a, wc_b) in word_counts(SIMPLE_CLEAN).zip(word_counts(SIMPLE_PLAIN)) {
             assert_eq!(
                 wc_a.3, wc_b.3,
                 "sura_number = {}, aya_number = {}, aya_text = {} and {}, word count = {} and {}",
@@ -39,6 +38,7 @@ mod tests {
     fn assert_same_basmalah(raw: &str) {
         let mut lines = raw.trim_start().split('\n').take_while(|l| !l.is_empty());
         let basmalah = lines.next().unwrap().split('|').nth(2).unwrap();
+        let basmalah = basmalah.to_owned() + " ";
         for line in lines {
             let mut parts = line.split('|');
             let sura_number: u8 = parts.next().unwrap().parse().unwrap();
@@ -46,7 +46,7 @@ mod tests {
             let aya_text = parts.next().unwrap();
             if aya_number == 1 && sura_number != 9 {
                 assert!(
-                    aya_text.starts_with(basmalah),
+                    aya_text.starts_with(&basmalah),
                     "sura_number = {}, aya_number = {}, aya_text = {}",
                     sura_number,
                     aya_number,
