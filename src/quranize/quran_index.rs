3f0e86d35f2b84a8d2e2a09f9d0423ba74d20d7e
Alfan Nur Fauzan <alpancs@gmail.com>
2022-07-02 22:01:29 +0700

    refactor module quranize

--- src/quranize/quran_index.rs
+++ src/quranize/quran_index.rs
@@ -1,3 +1,13 @@
+pub use HarfNode as Node;
+
+pub fn build_quran_index(word_count_limit: u8) -> Node {
+    let mut root = Node::new('\0');
+    for (s, a, t) in crate::quran::simple_clean_iter() {
+        root.update_tree(s, a, t, word_count_limit);
+    }
+    root
+}
+
 pub struct HarfNode {
     pub content: char,
     pub next_harfs: Vec<HarfNode>,
@@ -44,5 +54,26 @@ impl HarfNode {
                 self.next_harfs.last_mut().unwrap()
             }
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_build_quran_index() {
+        let root = build_quran_index(u8::MAX);
+        assert_eq!(root.content, '\0');
+        assert_eq!(root.next_harfs.len(), 31);
+        assert!(find_next(&root, 'ب').locations.is_empty());
+        assert_eq!(find_next(&root, 'ن').locations, vec![(68, 1, 1)]);
+    }
+
+    fn find_next(node: &Node, target: char) -> &Node {
+        node.next_harfs
+            .iter()
+            .find(|h| h.content == target)
+            .unwrap()
     }
 }
