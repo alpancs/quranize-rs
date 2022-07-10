4ec62779c18714948f0042ae827a7f8b8b765302
Alfan Nur Fauzan <alpancs@gmail.com>
2022-07-11 04:25:32 +0700

    add test cases

--- src/quranize/mod.rs
+++ src/quranize/mod.rs
@@ -95,6 +95,7 @@ mod tests {
         assert_eq!(encode(&q, "bismillah"), vec!["بسم الله", "بشماله"]);
         assert_eq!(encode(&q, "birobbinnas"), vec!["برب الناس"]);
         assert_eq!(encode(&q, "inna anzalnahu"), vec!["إنا أنزلناه"]);
+        assert_eq!(encode(&q, "wa'tasimu"), vec!["واعتصموا"]);
         assert_eq!(encode(&q, "wabarro"), vec!["وبرا", "وبئر"]);
         assert_eq!(encode(&q, "idza qodho"), vec!["إذا قضى"]);
         assert_eq!(encode(&q, "masyaallah"), vec!["ما شاء الله"]);
@@ -103,6 +104,7 @@ mod tests {
         assert_eq!(encode(&q, "innasya niaka"), vec!["إن شانئك"]);
         assert_eq!(encode(&q, "wasalamun alaihi"), vec!["وسلام عليه"]);
         assert_eq!(encode(&q, "ulaika hum"), vec!["أولئك هم"]);
+        assert_eq!(encode(&q, "waladdoolin"), vec!["ولا الضالين"]);
         assert_eq!(encode(&q, "n"), vec!["ن"]);
         assert_eq!(encode(&q, "qulhuwallahuahad").len(), 0);
     }
@@ -118,9 +120,22 @@ mod tests {
             encode(&q, "alhamdulilla hirobbil 'alamiin"),
             vec!["الحمد لله رب العالمين"]
         );
+        assert_eq!(encode(&q, "arrohma nirrohim"), vec!["الرحمن الرحيم"]);
+        assert_eq!(encode(&q, "maliki yau middin"), vec!["مالك يوم الدين"]);
         assert_eq!(
-            encode(&q, "wa'tasimu bihablillah"),
-            vec!["واعتصموا بحبل الله"]
+            encode(&q, "iyyakanakbudu waiyyakanastain"),
+            vec!["إياك نعبد وإياك نستعين"]
+        );
+        assert_eq!(
+            encode(&q, "ihdinassirotol mustaqim"),
+            vec!["اهدنا الصراط المستقيم"]
+        );
+        assert_eq!(
+            encode(
+                &q,
+                "shirotolladzina an'amta 'alaihim ghoiril maghdzubi 'alaihim waladdoolin"
+            ),
+            vec!["صراط الذين أنعمت عليهم غير المغضوب عليهم ولا الضالين"]
         );
         assert_eq!(encode(&q, "qulhuwallahuahad"), vec!["قل هو الله أحد"]);
     }
