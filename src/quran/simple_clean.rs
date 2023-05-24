697e7cc0e1b82d5bceaa3b03012748d8d0f44b34
Alfan Nur Fauzan <alpancs@gmail.com>
2023-05-24 22:25:07 +0700

    remove redundant lines

--- src/quran/simple_clean.rs
+++ src/quran/simple_clean.rs
@@ -1,32 +1,3 @@
-/**
-#====================================================================
-#
-#  Tanzil Quran Text (Simple Clean, Version 1.1)
-#  Copyright (C) 2007-2023 Tanzil Project
-#  License: Creative Commons Attribution 3.0
-#
-#  This copy of the Quran text is carefully produced, highly
-#  verified and continuously monitored by a group of specialists
-#  at Tanzil Project.
-#
-#  TERMS OF USE:
-#
-#  - Permission is granted to copy and distribute verbatim copies
-#    of this text, but CHANGING IT IS NOT ALLOWED.
-#
-#  - This Quran text can be used in any website or application,
-#    provided that its source (Tanzil Project) is clearly indicated,
-#    and a link is made to tanzil.net to enable users to keep
-#    track of changes.
-#
-#  - This copyright notice shall be included in all verbatim copies
-#    of the text, and shall be reproduced appropriately in all files
-#    derived from or containing substantial portion of this text.
-#
-#  Please check updates at: http://tanzil.net/updates/
-#
-#====================================================================
-*/
 #[cfg(test)]
 pub(crate) const RAW_QURAN: &str = r#"
 بسم الله الرحمن الرحيم
