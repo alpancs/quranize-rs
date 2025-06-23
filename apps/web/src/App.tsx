80696e0f2a32749788ca6d601614ba77f0543a14
Alfan Nur Fauzan <alpancs@gmail.com>
2025-06-23 11:36:03 +0700

    use CssBaseline

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import CssBaseline from '@mui/material/CssBaseline'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -8,6 +9,7 @@ function App() {
 
   return (
     <>
+      <CssBaseline />
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
