dec65ea8449a9854a08ace7469f42957a6582531
Alfan Nur Fauzan <alpancs@gmail.com>
2025-07-15 23:54:38 +0700

    add header using bulma css

---
+++ app/web/src/components/Header.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <header class="hero is-small is-info">
+    <div class="hero-body is-flex is-justify-content-center">
+      <span class="title">Quranize</span>
+    </div>
+  </header>
+</template>
+
+<style scoped></style>
