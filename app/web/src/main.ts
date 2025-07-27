f03718dbaece11b9f59e0e00ba6a2eb84df3e023
Alfan Nur Fauzan <alpancs@gmail.com>
2025-07-27 17:21:47 +0700

    register a service worker for PWA

--- app/web/src/main.ts
+++ app/web/src/main.ts
@@ -4,3 +4,9 @@ import './style.css';
 import App from './App.vue';
 
 createApp(App).use(router).mount('#app');
+
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/service-worker.js');
+    });
+}
