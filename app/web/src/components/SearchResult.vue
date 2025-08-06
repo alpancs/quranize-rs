d65e74ee4341e2713f1b200f3ed8b0cdd1be3f8b
Alfan Nur Fauzan <alpancs@gmail.com>
2025-08-06 23:27:20 +0700

    refactor: simplify SearchResult component structure and update styling

--- app/web/src/components/SearchResult.vue
+++ app/web/src/components/SearchResult.vue
@@ -2,38 +2,17 @@
 import { getSuraName, toArabicNumber } from '../utils/quranize';
 import type { SearchResult } from '../utils/types';
 import MarkedQuranText from '../components/MarkedQuranText.vue';
-import AyaNumber from '../components/AyaNumber.vue';
 
 defineProps<{ result: SearchResult }>();
 </script>
 
 <template>
-    <div class="card" dir="rtl">
-        <header class="card-header">
-            <p class="card-header-title">
-                <RouterLink :to="{ path: '/quran-page', query: result }" class="quran-text">
-                    {{ getSuraName(result.sura) }} : {{ toArabicNumber(result.aya) }}
-                </RouterLink>
-            </p>
-        </header>
-        <div class="card-content">
-            <div class="content">
-                <MarkedQuranText :beforeMarked="result.before_text" :marked="result.text"
-                    :afterMarked="result.after_text" />
-                <AyaNumber :aya="result.aya" />
-            </div>
-        </div>
+    <div class="box" dir="rtl">
+        <span class="quran-text pl-2">
+            <RouterLink :to="{ path: '/quran-page', query: result }" class="button">
+                {{ getSuraName(result.sura) }} : {{ toArabicNumber(result.aya) }}
+            </RouterLink>
+        </span>
+        <MarkedQuranText :beforeMarked="result.before_text" :marked="result.text" :afterMarked="result.after_text" />
     </div>
 </template>
-
-<style scoped>
-.card {
-    border-style: solid;
-    border-width: 1px;
-}
-
-.card-header {
-    border-bottom-style: dashed;
-    border-bottom-width: 1px;
-}
-</style>
