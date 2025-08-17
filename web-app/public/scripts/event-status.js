eee1dd58c54592e661ee536bc2cae53109d324cb
Alfan Nur Fauzan <alpancs@gmail.com>
2025-08-17 23:48:06 +0700

    remove web-app

--- web-app/public/scripts/event-status.js
+++
@@ -1,7 +0,0 @@
-export const EventStatus = Object.freeze({
-    WorkerInitiated: "WorkerInitiated",
-    KeywordUpdated: "KeywordUpdated",
-    KeywordEncoded: "KeywordEncoded",
-    ResultClicked: "ResultClicked",
-    ResultLocated: "ResultLocated",
-});
