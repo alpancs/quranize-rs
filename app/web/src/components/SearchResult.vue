8917d2ddb80e588fcde51e17237c944e7001e3d1
Alfan Nur Fauzan <alpancs@gmail.com>
2025-11-30 15:49:06 +0700

    refactor vue components (#46)
    * refactor
    
    * refactor
    
    * refactor home view
    
    * refactor home view
    
    * refactor search result view

--- app/web/src/components/SearchResult.vue
+++ app/web/src/components/SearchResult.vue
@@ -1,168 +1,22 @@
 <script setup lang="ts">
-import { inject, ref, watch } from "vue";
-import {
-    getPageItemGroups,
-    getSuraNameAR,
-    toArabicNumber,
-    type PageItem,
-} from "../utils/quranize";
-import type { SearchResult } from "../utils/types";
-import MarkedQuranText from "../components/MarkedQuranText.vue";
-import AyaNumber from "../components/AyaNumber.vue";
-import { useRouter, onBeforeRouteLeave } from "vue-router";
+import type { EncodeResult } from "../utils/types";
 
-const { result } = defineProps<{ result: SearchResult }>();
-
-const isTranslationVisible = ref(false);
-const isQuranPageVisible = ref(false);
-const pageItemGroups = ref<PageItem[][]>([]);
-const textID = ref("");
-
-inject<Function>("getTextID")?.(result.sura, result.aya).then(
-    (v: string) => (textID.value = v),
-);
-
-function toggleTranslationVisibility() {
-    isTranslationVisible.value = !isTranslationVisible.value;
-}
-
-function openQuranPage() {
-    isQuranPageVisible.value = true;
-}
-
-function closeQuranPage() {
-    isQuranPageVisible.value = false;
-}
-
-watch(
-    isQuranPageVisible,
-    async () => (pageItemGroups.value = await getPageItemGroups(result.page)),
-    { once: true },
-);
-
-const toQuranPage = {
-    name: "QuranPage",
-    params: { page: result.page },
-    query: { markedSura: result.sura, markedAya: result.aya },
-};
-
-const router = useRouter();
-
-function navToQuranPage(item: PageItem) {
-    router.push({
-        name: "QuranPage",
-        params: { page: result.page },
-        query: { markedSura: item.sura, markedAya: item.aya },
-    });
-}
-
-onBeforeRouteLeave((to) => {
-    if (isQuranPageVisible.value && to.name === "Home") {
-        closeQuranPage();
-        return false;
-    }
-});
+defineProps<{ result: EncodeResult }>();
 </script>
 
 <template>
-    <div class="card">
-        <header class="card-header" dir="rtl">
-            <p
-                class="card-header-title quran-text is-size-5 is-clickable"
-                @click="openQuranPage"
-            >
-                <span class="icon-text">
-                    <span class="icon">
-                        <font-awesome-icon icon="fa-solid fa-book-open" />
-                    </span>
-                    {{ getSuraNameAR(result.sura) }} :
-                    {{ toArabicNumber(result.aya) }}
-                </span>
+    <RouterLink
+        :to="{ name: 'SearchResult', query: result }"
+        class="box"
+        dir="rtl"
+    >
+        <div
+            class="is-flex is-justify-content-space-between is-align-items-center"
+        >
+            <p class="quran-text has-text-weight-semibold">
+                {{ result.quran }}
             </p>
-            <button
-                class="card-header-icon"
-                @click="toggleTranslationVisibility"
-            >
-                <span class="icon">
-                    <font-awesome-icon
-                        :icon="[
-                            'fas',
-                            isTranslationVisible ? 'angle-up' : 'angle-down',
-                        ]"
-                    />
-                </span>
-            </button>
-        </header>
-        <div class="card-content">
-            <div class="content">
-                <p
-                    dir="rtl"
-                    class="quran-text is-size-5 is-clickable"
-                    @click="toggleTranslationVisibility"
-                >
-                    <MarkedQuranText
-                        :beforeMarked="result.before_text"
-                        :marked="result.text"
-                        :afterMarked="result.after_text"
-                    />
-                </p>
-                <p v-if="isTranslationVisible">{{ textID }}</p>
-            </div>
+            <span class="tag is-rounded">{{ result.location_count }}</span>
         </div>
-    </div>
-
-    <div class="modal is-active" v-if="isQuranPageVisible">
-        <div class="modal-background" @click="closeQuranPage"></div>
-        <div class="modal-card">
-            <header class="modal-card-head mt-6">
-                <p class="modal-card-title">
-                    <RouterLink :to="toQuranPage" class="tag is-medium">
-                        <span class="icon">
-                            <font-awesome-icon
-                                icon="fa-solid fa-up-right-and-down-left-from-center"
-                            />
-                        </span>
-                    </RouterLink>
-                </p>
-                <button
-                    class="delete"
-                    aria-label="close"
-                    @click="closeQuranPage"
-                ></button>
-            </header>
-            <section class="modal-card-body">
-                <div
-                    dir="rtl"
-                    class="quran-text is-size-5"
-                    v-for="items in pageItemGroups"
-                >
-                    <p
-                        class="has-text-centered has-text-weight-semibold"
-                        v-if="items[0]?.aya === 1"
-                    >
-                        سورة {{ getSuraNameAR(items[0].sura) }}
-                    </p>
-                    <p class="has-text-justified">
-                        <span
-                            class="is-clickable"
-                            @click="navToQuranPage(item)"
-                            v-for="item in items"
-                        >
-                            <MarkedQuranText
-                                v-if="
-                                    item.sura === result.sura &&
-                                    item.aya === result.aya
-                                "
-                                :beforeMarked="result.before_text"
-                                :marked="result.text"
-                                :afterMarked="result.after_text"
-                            />
-                            <span v-else>{{ item.text }}</span>
-                            <AyaNumber :aya="item.aya" />
-                        </span>
-                    </p>
-                </div>
-            </section>
-        </div>
-    </div>
+    </RouterLink>
 </template>
--- app/web/src/views/HomeView.vue
+++ app/web/src/views/HomeView.vue
@@ -1,23 +1,19 @@
 <script setup lang="ts">
 import { ref, watch } from "vue";
 import { initiated, call } from "../utils/quranize";
-import type { EncodeResult as ER } from "../utils/types";
+import type { EncodeResult } from "../utils/types";
 import SearchBar from "../components/SearchBar.vue";
-import EncodeResult from "../components/EncodeResult.vue";
+import SearchResult from "../components/SearchResult.vue";
 
 const keyword = ref("");
-const encodeResults = ref<ER[]>([]);
+const results = ref<EncodeResult[]>([]);
 
-watch(
-    keyword,
-    async (text) => (encodeResults.value = await call("encode", text)),
-);
+watch(keyword, async (text) => (results.value = await call("encode", text)));
 </script>
 
 <template>
-    <div class="block">
-        <SearchBar v-model="keyword" />
-    </div>
+    <div class="block"><SearchBar v-model="keyword" /></div>
+
     <div class="skeleton-block" v-if="!initiated && keyword"></div>
-    <EncodeResult :result v-for="result in encodeResults" />
+    <SearchResult :result v-for="result in results" />
 </template>
