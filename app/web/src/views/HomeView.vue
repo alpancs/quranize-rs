cc253d2e565d8047d6220a772c5a10ffc2469b48
Alfan Nur Fauzan <alpancs@gmail.com>
2025-11-30 11:04:07 +0700

    refactor home view

--- app/web/src/views/HomeView.vue
+++ app/web/src/views/HomeView.vue
@@ -1,17 +1,14 @@
 <script setup lang="ts">
 import { ref, watch } from "vue";
 import { initiated, call } from "../utils/quranize";
-import type { EncodeResult as ER } from "../utils/types";
+import type { EncodeResult } from "../utils/types";
 import SearchBar from "../components/SearchBar.vue";
-import EncodeResult from "../components/EncodeResult.vue";
+import SearchResult from "../components/SearchResult.vue";
 
 const keyword = ref("");
-const encodeResults = ref<ER[]>([]);
+const results = ref<EncodeResult[]>([]);
 
-watch(
-    keyword,
-    async (text) => (encodeResults.value = await call("encode", text)),
-);
+watch(keyword, async (text) => (results.value = await call("encode", text)));
 </script>
 
 <template>
@@ -19,5 +16,5 @@ watch(
         <SearchBar v-model="keyword" />
     </div>
     <div class="skeleton-block" v-if="!initiated && keyword"></div>
-    <EncodeResult :result v-for="result in encodeResults" />
+    <SearchResult :result v-for="result in results" />
 </template>
