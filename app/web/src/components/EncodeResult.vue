c5d1697115fe98571d26a0517667b1b87ea8e42e
Alfan Nur Fauzan <alpancs@gmail.com>
2026-02-28 05:18:22 +0700

    remove unnecessary div

--- app/web/src/components/EncodeResult.vue
+++ app/web/src/components/EncodeResult.vue
@@ -7,15 +7,13 @@ defineProps<{ result: EncodeResult }>();
 
 <template>
     <RouterLink :to="{ name: 'SearchResult', query: { quran: result.quran } }" class="box">
-        <div class="is-flex is-flex-direction-column">
-            <div class="block is-flex is-justify-content-space-between is-align-items-center">
-                <span class="tag is-rounded">{{ result.location_count }}</span>
-                <p class="quran-text has-text-weight-semibold is-size-5-touch is-size-4-desktop">
-                    {{ result.quran }}
-                </p>
-            </div>
-
-            <ExplanationGroup :explanations="result.explanations" />
+        <div class="block is-flex is-justify-content-space-between is-align-items-center">
+            <span class="tag is-rounded">{{ result.location_count }}</span>
+            <p class="quran-text has-text-weight-semibold is-size-5-touch is-size-4-desktop">
+                {{ result.quran }}
+            </p>
         </div>
+
+        <ExplanationGroup :explanations="result.explanations" />
     </RouterLink>
 </template>
