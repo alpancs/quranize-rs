6f206d5c45676b42652401c4eebb2715e067146f
Alfan Nur Fauzan <alpancs@gmail.com>
2026-02-28 04:02:48 +0700

    move explanation to home view

--- app/web/src/components/EncodeResult.vue
+++ app/web/src/components/EncodeResult.vue
@@ -1,21 +1,40 @@
 <script setup lang="ts">
-import { computed } from "vue";
-import type { EncodeResult } from "../utils/types";
+import { computed, ref, watch } from "vue";
+import { call } from "../utils/quranize";
+import type { EncodeResult, Explanation } from "../utils/types";
 
-const { result } = defineProps<{ result: EncodeResult }>();
-const searchResultLink = computed(() => ({
-    name: "SearchResult",
-    query: { quran: result.quran, explanation: result.explanation },
-}));
+const props = defineProps<{ result: EncodeResult }>();
+const searchResultLink = computed(() => ({ name: "SearchResult", query: { quran: props.result.quran } }));
+const explanations = ref<Explanation[]>([]);
+
+watch(
+    () => props.result,
+    async (result) => {
+        explanations.value = await call<Explanation[]>("compressExpl", result.quran, result.explanation);
+    },
+    { immediate: true },
+);
 </script>
 
 <template>
-    <RouterLink :to="searchResultLink" class="box" dir="rtl">
-        <div class="is-flex is-justify-content-space-between is-align-items-center">
-            <p class="quran-text has-text-weight-semibold is-size-5-touch is-size-4-desktop">
-                {{ result.quran }}
-            </p>
-            <span class="tag is-rounded">{{ result.location_count }}</span>
+    <RouterLink :to="searchResultLink" class="box">
+        <div class="is-flex is-flex-direction-column">
+            <div class="is-flex is-justify-content-space-between is-align-items-center">
+                <span class="tag is-rounded">{{ result.location_count }}</span>
+                <p class="quran-text has-text-weight-semibold is-size-5-touch is-size-4-desktop">
+                    {{ result.quran }}
+                </p>
+            </div>
+            <div class="field is-grouped is-grouped-multiline is-justify-content-center">
+                <div class="control" v-for="expl in explanations">
+                    <div class="tags has-addons">
+                        <span class="tag is-info">{{ expl.alphabet }}</span>
+                        <span class="tag">
+                            <span class="quran-text">{{ expl.quran }}</span>
+                        </span>
+                    </div>
+                </div>
+            </div>
         </div>
     </RouterLink>
 </template>
--- app/web/src/views/SearchResultView.vue
+++ app/web/src/views/SearchResultView.vue
@@ -1,41 +1,27 @@
 <script setup lang="ts">
-import { ref } from "vue";
+import { ref, watch } from "vue";
 import { useRoute } from "vue-router";
 import { initiated, call } from "../utils/quranize";
-import type { SearchResult as SR, Explanation as Exp } from "../utils/types";
+import type { SearchResult } from "../utils/types";
 import SearchResultDetail from "../components/SearchResultDetail.vue";
 
 const route = useRoute();
-const { quran, explanation: expl } = route.query;
+const results = ref<SearchResult[]>([]);
 
-const results = ref<SR[]>([]);
-const explanations = ref<Exp[]>([]);
-
-call<SR[]>("getLocations", quran).then((v) => (results.value = v));
-call<Exp[]>("compressExpl", quran, expl).then((v) => (explanations.value = v));
+watch(
+    () => route.query.quran,
+    async (quran) => {
+        results.value = await call<SearchResult[]>("getLocations", quran);
+    },
+    { immediate: true },
+);
 </script>
 
 <template>
     <div class="block">
-
-        <div class="block">
-            <p class="quran-text has-text-centered has-text-weight-semibold is-size-4-touch is-size-3-desktop">
-                {{ quran }}
-            </p>
-        </div>
-
-        <div class="skeleton-block" v-if="!initiated"></div>
-        <div class="field is-grouped is-grouped-multiline is-justify-content-center">
-            <div class="control" v-for="e in explanations">
-                <div class="tags has-addons">
-                    <span class="tag is-info">{{ e.alphabet }}</span>
-                    <span class="tag">
-                        <span class="quran-text is-size-6">{{ e.quran }}</span>
-                    </span>
-                </div>
-            </div>
-        </div>
-
+        <p class="quran-text has-text-centered has-text-weight-semibold is-size-4-touch is-size-3-desktop">
+            {{ $route.query.quran }}
+        </p>
     </div>
 
     <div class="skeleton-block" v-if="!initiated"></div>
