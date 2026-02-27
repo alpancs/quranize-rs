df82989eb4799cb9685e2c6a85895e1edfaf63c0
Alfan Nur Fauzan <alpancs@gmail.com>
2026-02-28 04:05:42 +0700

    add missing spacing

--- app/web/src/components/EncodeResult.vue
+++ app/web/src/components/EncodeResult.vue
@@ -19,7 +19,7 @@ watch(
 <template>
     <RouterLink :to="searchResultLink" class="box">
         <div class="is-flex is-flex-direction-column">
-            <div class="is-flex is-justify-content-space-between is-align-items-center">
+            <div class="block is-flex is-justify-content-space-between is-align-items-center">
                 <span class="tag is-rounded">{{ result.location_count }}</span>
                 <p class="quran-text has-text-weight-semibold is-size-5-touch is-size-4-desktop">
                     {{ result.quran }}
