20f23e3957c2c0463e20a26355badeafeb15e5e8
Alfan Nur Fauzan <alpancs@gmail.com>
2026-02-28 07:33:08 +0700

    bug fix import path

--- app/web/src/utils/translation.ts
+++ app/web/src/utils/translation.ts
@@ -1,6 +1,6 @@
 const textIDMap = (async function () {
   const map = new Map<string, string>();
-  const plainTextID = (await import("./utils/quran/id.indonesian.txt?raw")).default;
+  const plainTextID = (await import("./quran/id.indonesian.txt?raw")).default;
   plainTextID.split("\n").forEach((line) => {
     const split = line.split("|");
     if (split.length === 3) {
