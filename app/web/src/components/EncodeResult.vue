77c9a3b2f0aeb8675902890f432bb653559cac06
Alfan Nur Fauzan <alpancs@gmail.com>
2026-03-01 14:45:13 +0700

    encode-decode quran in URL query

--- app/web/src/components/EncodeResult.vue
+++ app/web/src/components/EncodeResult.vue
@@ -1,11 +1,18 @@
 <script setup lang="ts">
 import ExplanationGroup from "../components/ExplanationGroup.vue";
 import type { EncodeResult } from "../utils/types";
+
+function encode(str: string): string {
+    const bytes = new TextEncoder().encode(str);
+    const binString = String.fromCodePoint(...bytes);
+    return btoa(binString);
+}
+
 defineProps<{ result: EncodeResult }>();
 </script>
 
 <template>
-    <RouterLink class="box" :to="{ name: 'SearchResult', query: { quran: result.quran } }">
+    <RouterLink class="box" :to="{ name: 'SearchResult', query: { code: encode(result.quran) } }">
         <div class="block is-flex is-justify-content-space-between is-align-items-center">
             <span class="tag is-rounded">{{ result.location_count }}</span>
             <p class="quran-text has-text-weight-semibold is-size-5-touch is-size-4-desktop">
--- app/web/src/views/SearchResultView.vue
+++ app/web/src/views/SearchResultView.vue
@@ -5,15 +5,22 @@ import SearchResultDetail from "../components/SearchResultDetail.vue";
 import { call, initiated } from "../utils/quranize";
 import type { SearchResult } from "../utils/types";
 
+function decode(str: string): string {
+    const binString = atob(str);
+    const bytes = Uint8Array.from(binString, (m) => m.codePointAt(0)!);
+    return new TextDecoder().decode(bytes);
+}
+
 const route = useRoute();
+const quran = decode(route.query.code as string);
 const results = ref<SearchResult[]>([]);
-call<SearchResult[]>("getLocations", route.query.quran).then((res) => (results.value = res));
+call<SearchResult[]>("getLocations", quran).then((res) => results.value = res);
 </script>
 
 <template>
     <div class="block">
         <p class="quran-text has-text-centered has-text-weight-semibold is-size-4-touch is-size-3-desktop">
-            {{ $route.query.quran }}
+            {{ quran }}
         </p>
     </div>
 
