9e0cb9d55f45c76699a0659a4917746914caf985
Alfan Nur Fauzan <alpancs@gmail.com>
2026-03-02 06:22:29 +0700

    fix test: test_quranize_default

--- quranize/src/transliteration/mod.rs
+++ quranize/src/transliteration/mod.rs
@@ -77,8 +77,11 @@ pub(super) fn contextual_map(context: Option<char>, c: char) -> &'static [&'stat
         (Some(ARABIC_FATHATAN), ARABIC_LETTER_ALEF_MAKSURA) => &[""],
         (Some(ARABIC_KASRA), ARABIC_LETTER_LAM) => &[""],
         (Some(ARABIC_LETTER_ALEF_MAKSURA), ARABIC_LETTER_SUPERSCRIPT_ALEF) => &[""],
+        (Some(ARABIC_FATHA), ARABIC_LETTER_SUPERSCRIPT_ALEF) => &[""],
+        (Some(ARABIC_KASRA), ARABIC_LETTER_YEH) => &[""],
+        (Some(ARABIC_LETTER_MEEM), ARABIC_SHADDA) => &[""],
         (Some(ARABIC_LETTER_ALEF), ARABIC_LETTER_LAM) => &[""],
-        (Some(ARABIC_LETTER_JEEM), ARABIC_LETTER_REH) => &["re", "ree"],
+        (Some(ARABIC_LETTER_REH), ARABIC_FATHA) => &["e", "ee"],
 
         (Some(c), ARABIC_SHADDA) => map(c),
 
